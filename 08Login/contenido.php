cb14d32c1fe230284ab1621bb58e2696f27e6ee5
guillermo.spindola@sm4rt.com <gspindolab@gmail.com>
2018-09-24 17:35:37 -0500

    Jotos todos

---
+++ 08Login/contenido.php
@@ -0,0 +1,5 @@
+<?php
+
+include_once 'views/contenido.view.php';
+
+?>
---
+++ 08Login/views/contenido.view.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/estilos.css">
+    <title>Contenido</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 titulo mt-5">
+                <h1 class="text-center">Contenido del Sitio</h1>
+            </div>
+        </div>
+        <div class="row justify-content-center mt-5">
+            <div class="col-12 col-md-6 col-lg-4">
+                <a href="cerrar.php">Cerrar Sesión</a>
+                <hr>
+                <article class="bg-white p-3">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum eaque nemo cumque. Dolorem, consequuntur? Unde quod omnis dolor molestias. Nisi ratione molestias eos officiis eum labore officia porro libero deleniti!</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad velit cum saepe, distinctio eveniet amet nobis maiores placeat qui, iure itaque accusantium quod vero, quo dignissimos molestiae recusandae voluptatibus fugiat!</p>
+                </article>
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</body>
+</html>
