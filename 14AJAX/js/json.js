334218b2798f9039915414f614734d409d1edc8f
Guillermo Spindola <gspindolab@gmail.com>
2019-01-11 13:51:25 -0600

    Ejemplo de JSON

---
+++ 14AJAX/js/json.js
@@ -0,0 +1,47 @@
+//var nombre = 'Guillermo';
+//var edad = 24;
+//var pais = 'Mexico';
+//
+//var nombre2 = 'Josh';
+//var edad2 = 25;
+//var pais2 = 'USA';
+
+//Ejemplo de un Objetp
+/*var Guillermo = {
+    "nombre" : "Guillermo Spindola",
+    "edad" : 24,
+    "pais" : "Mexico"
+}
+
+var Josh = {
+    "nombre" : "Josh Nickolz",
+    "edad" : 25,
+    "pais" : "USA"
+}*/
+
+//Acceder a cada uno de los elementos del objeto/
+//console.log(Josh.nombre);
+
+//Definicion de un arreglo
+var primos = ['Yahir', 'Alejandro'];
+//Acceder al arreglo en su posicion 1 
+console.log(primos[0]);
+
+//Un archivo JSON es un arreglo que contiene objetos.
+//EJEMPLO:
+var amigos = [
+    {
+        "nombre" : "Yahir",
+        "edad" : 27,
+        "pais" : "Mexico"
+    },
+    {
+        "nombre" : "Alejandro",
+        "edad" : 25,
+        "pais" : "Mexico"
+    }
+];
+
+//Acceder al JSON
+console.log(amigos[0].pais);
+
